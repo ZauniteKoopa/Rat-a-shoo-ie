df11b4204dc0367efe3de03c43ad1c2390d294e2
Anthony Hoang <anthonyhoang3074@gmail.com>
2021-09-01 13:04:01 -0500

    First Commit

---
+++ Assets/Scripts/Rat/FeetSensor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FeetSensor : MonoBehaviour
+{
+    // Event handling
+    private int numTouchingObjects = 0;
+    public UnityEvent landingEvent;
+    public UnityEvent leavingEvent;
+
+    // On trigger enter 2D, check if object is a platform. If so, alert landing event
+    void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.tag == "Platform") {
+            if (numTouchingObjects == 0) {
+                landingEvent.Invoke();
+            }
+
+            numTouchingObjects++;
+        }
+    }
+
+    // On trigger exit 2D, check if object is a platform, If so, alert falling event
+    void OnTriggerExit2D(Collider2D collider) {
+        if (collider.tag == "Platform") {
+            numTouchingObjects--;
+
+            if (numTouchingObjects == 0) {
+                leavingEvent.Invoke();
+            }
+        }
+    }
+
+    // Public function to check if the feet is on ground or not
+    public bool isGrounded() {
+        return numTouchingObjects > 0;
+    }
+}
---
+++ Assets/Scripts/Rat/RatController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RatController : MonoBehaviour
+{
+    // Editable properties
+    [SerializeField]
+    private float landSpeed = 7f;
+    [SerializeField]
+    private float airControl = 0.75f;
+    [SerializeField]
+    private float landJumpVelocity = 18f;
+    [SerializeField]
+    private float airJumpVelocity = 10f;
+    [SerializeField]
+    private int maxJumps = 2;
+
+    // Flags for jumping
+    private int jumpsLeft;
+    private bool onGround = false;
+
+    // Reference variables
+    private Rigidbody2D rigidBody;
+
+
+    /* Start is called before the first frame update */
+    void Start()
+    {
+        jumpsLeft = maxJumps;
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    /* Update is called every frame */
+    void Update()
+    {
+        // Handles horizontal Velocity
+        Vector3 horizontalVelocity = Input.GetAxis("Horizontal") * Vector3.right;
+        horizontalVelocity *= (landSpeed * Time.deltaTime);
+        if (!onGround) {
+            horizontalVelocity *= airControl;
+        }
+
+        transform.Translate(horizontalVelocity);
+
+        // Handles jump
+        if (jumpsLeft > 0 && Input.GetButtonDown("Jump")) {
+            float curJumpVelocity = (onGround) ? landJumpVelocity : airJumpVelocity;
+            rigidBody.velocity = Vector3.up * curJumpVelocity;
+            jumpsLeft--;
+        }
+    }
+
+    /* Event handler when rat lands on ground */
+    public void onLanding() {
+        onGround = true;
+        jumpsLeft = maxJumps;
+    }
+
+    /* Event handler method when rat leaves the ground */
+    public void onLeavingGround() {
+        onGround = false;
+        jumpsLeft = maxJumps - 1;
+    }
+}
