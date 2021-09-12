7625b517925f5717f741932b995eddd7f4b07f41
Anthony Hoang <anthonyhoang3074@gmail.com>
2021-09-12 13:04:55 -0500

    Organized scripts folder and made all sprites face the camera at all times

---
+++ Assets/Scripts/Camera/WorldSprite.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldSprite : MonoBehaviour
+{
+
+    // Update is called once per frame: make sure the sprite is always facing the camera
+    void Update()
+    {
+        transform.forward = (transform.position - Camera.main.transform.position).normalized;
+    }
+}
