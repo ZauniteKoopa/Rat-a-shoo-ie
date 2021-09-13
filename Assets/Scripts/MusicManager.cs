0546e723efab0e6940dde788eff0f91304f06b59
Ale-Acevedo <hectorbacevedo@utexas.edu>
2021-09-13 18:49:43 -0500

    Added tutorial to build index, created musicPlayer, implemented, set up infrastructure for dynamic music

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] tracks = null;
+    public AudioSource baseMusic;
+    public AudioSource chaseMusic;
+
+    private void Awake()
+    {
+        int curLevel = SceneManager.GetActiveScene().buildIndex;
+        int curChaseLevel = SceneManager.GetActiveScene().buildIndex + SceneManager.sceneCountInBuildSettings;
+        print("level" + curLevel);
+        AudioClip baseLevel = tracks[curLevel];
+        //AudioClip chaseLevel = tracks[curLevel + SceneManager.sceneCountInBuildSettings];
+        print("chase index" + curChaseLevel);
+
+        baseMusic.clip = baseLevel;
+        baseMusic.Play();
+
+        //chaseMusic.clip = chaseLevel;
+        //chaseMusic.volume = 0.0f;
+        //chaseMusic.Play();
+    }
+}
