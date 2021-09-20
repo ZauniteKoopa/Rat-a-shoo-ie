c376bbf6866e00d25305b56f800a66d4efd1ad1a
Ale-Acevedo <hectorbacevedo@utexas.edu>
2021-09-20 18:32:31 -0500

    SFX implementation, music needs fixing

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -6,24 +6,63 @@ using UnityEngine.SceneManagement;
 public class MusicManager : MonoBehaviour
 {
     [SerializeField]
-    private AudioClip[] tracks = null;
+    private AudioClip[] mainTracks = null;
+    [SerializeField]
+    private AudioClip[] chaseTracks = null;
     public AudioSource baseMusic;
     public AudioSource chaseMusic;
+
+    public float fadeOutFactor = 0.5f;
+    public float fadeInFactor = 0.5f;
+    public float maxVolume = 1f;
+    private bool fadeInOut = false;
 
     private void Awake()
     {
         int curLevel = SceneManager.GetActiveScene().buildIndex;
-        int curChaseLevel = SceneManager.GetActiveScene().buildIndex + SceneManager.sceneCountInBuildSettings;
         print("level" + curLevel);
-        AudioClip baseLevel = tracks[curLevel];
-        //AudioClip chaseLevel = tracks[curLevel + SceneManager.sceneCountInBuildSettings];
-        print("chase index" + curChaseLevel);
+        AudioClip baseLevel = mainTracks[curLevel];
+        AudioClip chaseLevel = chaseTracks[curLevel];
 
         baseMusic.clip = baseLevel;
         baseMusic.Play();
 
-        //chaseMusic.clip = chaseLevel;
-        //chaseMusic.volume = 0.0f;
-        //chaseMusic.Play();
+        chaseMusic.clip = chaseLevel;
+        chaseMusic.volume = 0.0f;
+        chaseMusic.Play();
+    }
+
+    private void Update()
+    {
+        Debug.Log(fadeInOut);
+        if (fadeInOut)
+        {
+            Debug.Log("update true");
+            if (chaseMusic.volume < maxVolume)
+            {
+                chaseMusic.volume += fadeInFactor * Time.deltaTime;
+            }
+        }
+
+        if (!fadeInOut)
+        {
+            //Debug.Log("update false");
+            if (chaseMusic.volume > 0.0f)
+            {
+                chaseMusic.volume -= fadeOutFactor * Time.deltaTime;
+            }
+        }
+    }
+
+    public void ratIsSeen()
+    {
+        fadeInOut = true;
+        Debug.Log("seen and chasing");
+    }
+
+    public void ratNotSeen()
+    {
+        fadeInOut = false;
+        Debug.Log("safe");
     }
 }
