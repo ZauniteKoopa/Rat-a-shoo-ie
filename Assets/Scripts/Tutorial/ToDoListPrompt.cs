1192f112227a4d12932ee39ae137e05afc89954d
Anthony Hoang <anthonyhoang3074@gmail.com>
2021-10-02 20:34:40 -0500

    Fixed bug concerning todo list prompt and finally edited numbers to our liking for movement (player and chef)

--- Assets/Scripts/Tutorial/ToDoListPrompt.cs
+++ Assets/Scripts/Tutorial/ToDoListPrompt.cs
@@ -7,7 +7,7 @@ public class ToDoListPrompt : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire3")) {
+        if (Input.GetButtonDown("TaskList")) {
             Object.Destroy(gameObject);
         } 
     }
