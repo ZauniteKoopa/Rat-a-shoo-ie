1fc35080c7ca2a2bcab86daca705fd9a40411185
Anthony Hoang <anthonyhoang3074@gmail.com>
2021-10-31 11:47:40 -0500

    Fixed trailers breaking pillars bug and nerfed HibachiChef AOE hitbox

---
+++ Assets/Scripts/Chef/Attacks/BasicHitbox.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicHitbox : MonoBehaviour
+{
+    // Method to check for player collision
+    private void OnTriggerEnter(Collider collider) {
+        RatController3D ratPlayer = collider.GetComponent<RatController3D>();
+
+        if (ratPlayer != null) {
+            ratPlayer.takeDamage();
+        }
+    }
+}
