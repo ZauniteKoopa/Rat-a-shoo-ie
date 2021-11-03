0e661d39431d7776457c2a496fc54ce4da013bf8
Ale-Acevedo <hectorbacevedo@utexas.edu>
2021-11-02 20:11:23 -0600

    Hibachi audio events, hibachi UI, pillar, and new sushi music implemented

---
+++ Assets/HibachiEventAudioManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HibachiEventAudioManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip reveal = null;
+    [SerializeField]
+    private AudioClip damage = null;
+    [SerializeField]
+    private AudioClip collapse = null;
+
+    private AudioSource speaker;
+
+    void Awake()
+    {
+        speaker = GetComponent<AudioSource>();
+    }
+
+    public void pillarReveal()
+    {
+        Debug.Log("reveal");
+        speaker.PlayOneShot(reveal);
+    }
+
+    public void pillarDamage()
+    {
+        speaker.PlayOneShot(damage);
+    }
+
+    public void endCollapse()
+    {
+        speaker.PlayOneShot(collapse);
+    }
+}
