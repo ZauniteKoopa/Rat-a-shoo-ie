a5f934ca1e831b0332b41135365f8d97dc6e48bf
Anthony Hoang <anthonyhoang3074@gmail.com>
2021-11-15 10:08:01 -0600

    Made dynamic chef hitbox that can either burn, break, or both

--- Assets/Scripts/Chef/ChefHitbox.cs
+++ Assets/Scripts/Chef/ChefHitbox.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class ChefHitbox : MonoBehaviour
 {
+    [SerializeField]
+    private bool canBreak = true;
+    [SerializeField]
+    private bool canBurn = false;
+
     // Method to check for player collision
     private void OnTriggerEnter(Collider collider) {
         RatController3D ratPlayer = collider.GetComponent<RatController3D>();
@@ -12,8 +17,13 @@ public class ChefHitbox : MonoBehaviour
 
         if (ratPlayer != null) {
             ratPlayer.takeDamage();
-        } else if (destroyableObject != null && destroyableObject.breakable) {
-            destroyableObject.destroyObject();
+        } else if (destroyableObject != null) {
+            bool willBreak = canBreak && destroyableObject.breakable;
+            bool willBurn = canBurn && destroyableObject.burnable;
+
+            if (willBreak || willBurn) {
+                destroyableObject.destroyObject();
+            }
         } else if (pillar != null && pillar.canDestroy()) {
             pillar.destroy();
         }
