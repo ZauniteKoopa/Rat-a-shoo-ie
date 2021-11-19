cdb6eedf6252bb72b1d309af29dd50fa0beaaa4f
Anthony Hoang <anthonyhoang3074@gmail.com>
2021-11-18 18:56:37 -0600

    Added build platform changers for icons and text - intro cutscene and tutorial

---
+++ Assets/Scripts/MobileControls/PlatformIconChanger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformIconChanger : MonoBehaviour
+{
+    // Main GameObject to make public
+    [SerializeField]
+    private GameObject androidIcon = null;
+    [SerializeField]
+    private GameObject computerIcon = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // If on android, enable android on screen controls
+        if (Application.platform == RuntimePlatform.Android) {
+            androidIcon.SetActive(true);
+            computerIcon.SetActive(false);
+        } else {
+            computerIcon.SetActive(true);
+            androidIcon.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/MobileControls/PlatformTextChanger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlatformTextChanger : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text textBox = null;
+    [SerializeField]
+    private string pcText = null;
+    [SerializeField]
+    private string androidText = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textBox.text = (Application.platform == RuntimePlatform.Android) ? androidText : pcText;
+    }
+}
