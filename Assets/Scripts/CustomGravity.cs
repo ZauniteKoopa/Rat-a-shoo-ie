35349e2d19fc74ce25bc7123e7604575ca607ca2
Anthony Hoang <anthonyhoang3074@gmail.com>
2021-11-30 11:02:11 -0600

    Gave bottle in last room of tutorial custom gravity

---
+++ Assets/Scripts/CustomGravity.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomGravity : MonoBehaviour
+{
+    [SerializeField]
+    private float customGravity = 9.81f;
+    [SerializeField]
+    private bool gravityEnabled = true;
+    private Rigidbody rb;
+
+    // On start, set reference variable to this object's rigidbody
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // On fixed update, apply gravity
+    private void FixedUpdate() {
+        if (gravityEnabled) {
+            rb.AddForce(rb.mass * customGravity * Vector3.down);
+        }
+    }
+}
