3997ed50c922511d7be07917b1f1afb95845d502
Anthony Hoang <anthonyhoang3074@gmail.com>
2021-11-29 19:27:36 -0600

    Got rid of magic number timings, made customer order windows very scalable, made sure they work

--- Assets/Scripts/Chef/OrderInfo/OrderWindow.cs
+++ Assets/Scripts/Chef/OrderInfo/OrderWindow.cs
@@ -2,25 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Assertions;
 
-public enum CustomerStatus {
-    WAITING,
-    CONTENT,
-    POISONED
+// Main customer package class
+[System.Serializable]
+public class CustomerPackage {
+    public Sprite waitingSprite;
+    public Sprite happySprite;
+    public Sprite poisonedSprite;
 }
 
+// Main Order Window class
 public class OrderWindow : MonoBehaviour
 {
     private Recipe currentRecipe = null;
     private LevelInfo levelInfo = null;
     private OrderWindowAudioManager audioManager = null;
 
-    private CustomerStatus customerStatus = CustomerStatus.WAITING;
     public UnityEvent poisonMealEvent;
+    [Header("Customer Sprites")]
     [SerializeField]
-    private SpriteRenderer customerSprite = null;
+    private SpriteRenderer customerSpriteRender = null;
     [SerializeField]
-    private Sprite[] customer1Sprites = null;
+    private List<CustomerPackage> customerSprites = null;
+    private int curCustomerIndex = 0;
+
+    [Header("Customer Timings")]
+    [SerializeField]
+    private float happyDuration = 0.4f;
+    [SerializeField]
+    private float customerLeaveDuration = 0.3f;
+    [SerializeField]
+    private float emptyCustomerWindowDuration = 0.5f;
+    [SerializeField]
+    private float customerBarfDuration = 0.5f;
+    [SerializeField]
+    private float customerDeathDuration = 0.5f;
 
     // On awake, get a recipe from the level info
     private void Awake() {
@@ -29,8 +46,15 @@ public class OrderWindow : MonoBehaviour
         audioManager = GetComponent<OrderWindowAudioManager>();
 
         // Initializing customer
-        customerStatus = CustomerStatus.WAITING;
-        customerSprite.sprite = customer1Sprites[(int)customerStatus];
+        Debug.Assert(customerSprites.Count > 0);
+        curCustomerIndex = Random.Range(0, customerSprites.Count);
+        customerSpriteRender.sprite = customerSprites[curCustomerIndex].waitingSprite;
+
+        // Connect to User interface event
+        ToDoList toDoList = FindObjectOfType<ToDoList>();
+        if (toDoList != null) {
+            toDoList.playerFinishAllTasksEvent.AddListener(onToDoListDone);
+        }
     }
     
     // Public method to serve food to potential customers
@@ -49,45 +73,51 @@ public class OrderWindow : MonoBehaviour
 
     // Main sequence to switch to new customer
     private IEnumerator switchToNewCustomer() {
-        customerStatus = CustomerStatus.CONTENT;
-        customerSprite.sprite = customer1Sprites[(int)customerStatus];
+        customerSpriteRender.sprite = customerSprites[curCustomerIndex].happySprite;
 
-        yield return new WaitForSeconds(0.2f);
+        yield return new WaitForSeconds(happyDuration);
 
         float timer = 0.0f;
-        while (timer < 0.3f) {
+        while (timer < customerLeaveDuration) {
             yield return null;
 
             timer += Time.deltaTime;
-            customerSprite.color = Color.Lerp(Color.white, Color.clear, timer / 0.3f);
+            customerSpriteRender.color = Color.Lerp(Color.white, Color.clear, timer / customerLeaveDuration);
         }
 
-        yield return new WaitForSeconds(0.1f);
-
-        customerSprite.color = Color.white;
+        yield return new WaitForSeconds(emptyCustomerWindowDuration);
 
         // Switch to new customer here
-        customerStatus = CustomerStatus.WAITING;
-        customerSprite.sprite = customer1Sprites[(int)customerStatus];
+        curCustomerIndex = Random.Range(0, customerSprites.Count);
+        customerSpriteRender.sprite = customerSprites[curCustomerIndex].waitingSprite;
+        customerSpriteRender.color = Color.white;
     }
 
     // Main sequence to do customer poisoned animation
     private IEnumerator poisonCustomerSequence() {
-        customerStatus = CustomerStatus.POISONED;
-        customerSprite.sprite = customer1Sprites[(int)customerStatus];
-        customerSprite.color = Color.white;
+        customerSpriteRender.sprite = customerSprites[curCustomerIndex].waitingSprite;
+        customerSpriteRender.color = Color.white;
+
+        yield return new WaitForSeconds(customerBarfDuration);
 
         // customer dies here
         float timer = 0.0f;
-        while (timer < 0.5f) {
+        while (timer < customerDeathDuration) {
             yield return null;
             timer += Time.deltaTime;
-            customerSprite.color = Color.Lerp(Color.white, Color.clear, timer / 0.5f);
+            customerSpriteRender.color = Color.Lerp(Color.white, Color.clear, timer / customerDeathDuration);
         }
     }
 
     // Public method to access the current recipe
     public Recipe getCurrentRecipe() {
         return currentRecipe;
+    }
+
+    // On to do list done
+    //  all order windows must barf in unison
+    public void onToDoListDone() {
+        StopAllCoroutines();
+        StartCoroutine(poisonCustomerSequence());
     }
 }
